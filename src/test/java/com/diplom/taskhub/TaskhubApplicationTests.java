dc822fc1761b450292c2f1fd97a09de0bcdc8e4f
Madiyar Zhanzhigitov <klintzhan005@gmail.com>
2025-02-25 11:55:08 +0500

    asd

---
+++ src/test/java/com/diplom/taskhub/TaskhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diplom.taskhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
